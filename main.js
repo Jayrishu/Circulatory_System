8bc265fa39728af52519005eaca1bd89be23f11d
Jayrishu <91966368+Jayrishu@users.noreply.github.com>
2022-10-11 17:36:02 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function composition(){
+    location.href="composition.html"
+}
+function back(){
+    location.href="index.html"
+}
+function parts(){
+    location.href="part.html"
+}
+function partsheart(){
+    location.href="hartheart.html"
+}
